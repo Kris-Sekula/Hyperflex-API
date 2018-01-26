1b2ef2fc58a9890930560f39636a3aaac0d87eae
Kris-Sekula <ksekula@cisco.com>
2018-01-26 12:38:52 +0000

    Added logging and error handling

--- hx_metrics.py
+++ hx_metrics.py
@@ -7,6 +7,10 @@ import json
 import hx_creds
 import time
 from gevent.wsgi import WSGIServer
+#
+import logging
+import logging.handlers
+#
 
 '''
 the hx_creds.py looks like this:
@@ -14,6 +18,10 @@ the hx_creds.py looks like this:
 hosts=[{'host':'10.1.1.1', 'username':'local/root', 'password':'*******'},{'host':'10.1.1.2', 'username':'local/root', 'password':'****'}]
 
 '''
+# Logging config
+logFile="hx_stats.log"
+logCount=4
+logBytes=1048576
 
 # suppress the unverified request messages (when using self-signed certificates)
 requests.packages.urllib3.disable_warnings()
@@ -30,43 +38,72 @@ def get_stats():
 	for host in hx_creds.hosts:
 		# get auth token (either cached or new one)
 		authdata = get_auth(host['host'], host['username'], host['password'])
-	        
+		if not authdata:
+			logger.info("Missing token, skipping host: "+host['host'])
+			continue
 		url = "https://"+host['host']
-		# url to get bandwidth data, we only look at the last 5 min (this will bring 5 records)
+		
+                # uri for throughput data with "last 5 min" filter
 		uri_MBps = '/render?target=stats.counters.scvmclient.allhosts.nfsBytesRead.cluster.rate&target=stats.counters.scvmclient.allhosts.nfsBytesWritten.cluster.rate&format=json&from=-5min'
-		# url to get the IOPS data 
-		uri_IOPS = '/render?target=stats.counters.scvmclient.allhosts.nfsReads.cluster.rate&target=stats.counters.scvmclient.allhosts.nfsWrites.cluster.rate&format=json&from=-5min'
+		# Get throughput data
+		MBps_data = get_stats(authdata,url+uri_MBps)
+		if MBps_data:
+			try:
+				MBps_Read=round(MBps_data[0]['datapoints'][-2][0],3)
+                		MBps_Write=round(MBps_data[1]['datapoints'][-2][0],3)
+			
+				# build the results
+                		results += 'MBps_Read{host="%s"} %s\n'%(host['host'],str(MBps_Read))
+                		results += 'MBps_Write{host="%s"} %s\n'%(host['host'],str(MBps_Write))
+			except:
+				logger.error("Couldn't parse returned throughput data")
+				pass
+
+		# url to get the IOPS data
+                uri_IOPS = '/render?target=stats.counters.scvmclient.allhosts.nfsReads.cluster.rate&target=stats.counters.scvmclient.allhosts.nfsWrites.cluster.rate&format=json&from=-5min'
+		# get IOPS data
+		IOPS_data = get_stats(authdata,url+uri_IOPS)
+		if IOPS_data:
+			try:
+				IOPS_Read=round(IOPS_data[0]['datapoints'][-2][0],3)
+                		IOPS_Write=round(IOPS_data[1]['datapoints'][-2][0],3)
+			
+				# build the results
+ 				results += 'IOPS_Read{host="%s"} %s\n'%(host['host'],str(IOPS_Read))
+                		results += 'IOPS_Write{host="%s"} %s\n'%(host['host'],str(IOPS_Write))
+			except:
+				logger.error("Couldn't parse returned IOPS data")
+                                pass
+
 		# url to get Latency data
 		uri_Lat ='/render?target=divideSeries(stats.timers.scvmclient.allhosts.nfsReadLatency.cluster.total%2Cstats.counters.scvmclient.allhosts.nfsReads.cluster.count)&target=divideSeries(stats.timers.scvmclient.allhosts.nfsWriteLatency.cluster.total%2Cstats.counters.scvmclient.allhosts.nfsWrites.cluster.count)&format=json&from=-5min'
-		# get data
-		MBps_data = get_stats(authdata,url+uri_MBps)
-		IOPS_data = get_stats(authdata,url+uri_IOPS)
+
+		# get latency data
 		Lat_data  = get_stats(authdata,url+uri_Lat)
-		# extract data [-2] as we are taking one before last record, sometime last in "None" so I skip it	
-		MBps_Read=round(MBps_data[0]['datapoints'][-2][0],3)
-        	MBps_Write=round(MBps_data[1]['datapoints'][-2][0],3)
-	
-		IOPS_Read=round(IOPS_data[0]['datapoints'][-2][0],3)
-        	IOPS_Write=round(IOPS_data[1]['datapoints'][-2][0],3)
-		
-		Lat_Read=round(Lat_data[0]['datapoints'][-2][0],3)
-		Lat_Write=round(Lat_data[1]['datapoints'][-2][0],3)
-		
-		# build the results
-		results += 'MBps_Read{host="%s"} %s\n'%(host['host'],str(MBps_Read))
-		results += 'MBps_Write{host="%s"} %s\n'%(host['host'],str(MBps_Write))
-		
-		results += 'IOPS_Read{host="%s"} %s\n'%(host['host'],str(IOPS_Read))
-		results += 'IOPS_Write{host="%s"} %s\n'%(host['host'],str(IOPS_Write))
-		
-		results += 'Lat_Read{host="%s"} %s\n'%(host['host'],str(Lat_Read))
-                results += 'Lat_Write{host="%s"} %s\n'%(host['host'],str(Lat_Write))
+		if Lat_data:
+			try:
+				Lat_Read=round(Lat_data[0]['datapoints'][-2][0],3)
+                		Lat_Write=round(Lat_data[1]['datapoints'][-2][0],3)		
+			
+				# build the results			
+				results += 'Lat_Read{host="%s"} %s\n'%(host['host'],str(Lat_Read))
+                		results += 'Lat_Write{host="%s"} %s\n'%(host['host'],str(Lat_Write))
+			except:
+				logger.error("Couldn't parse returned latency data")
+                                pass
+		#
+		#  When processing data I'm taking one before last record ([-2]), as sometimes the last record is None	
+		#
 	# return the results to the caller
 	return Response(results, mimetype='text/plain')
 
-
+#
+# Returns Authentication token
+#
 def get_auth(host, username, password):
-	print "call get_auth"	
+	
+	logger.info("Trying to auth "+host)
+
 	global tokens
 
 	headers={'content-type':'application/json'}
@@ -78,7 +115,6 @@ def get_auth(host, username, password):
 		"redirect_uri": "http://"+host
 	}
 
-	url = 'https://%s/aaa/v1/auth?grant_type=password'%host
 
 	if tokens.get(host):
                 # looks like we have token cached already
@@ -94,13 +130,14 @@ def get_auth(host, username, password):
                 	url = 'https://%s/aaa/v1/validate'%host
 			response = requests.post(url,headers=headers,data=json.dumps(payload),verify=False,timeout=4)
 			if response.status_code == 200:
-				print "Re-using token"
+				logger.info("Re-using existing auth token")
                                 return tokens.get(host)
-                	print "failed to validate"        	
+                	logger.error("Failed to validate existing token "+response.content)        	
 		except Exception as e:
-			print "failed to post validate",e	
-	# this happens if no cached taken found
+			logger.error("Post for token validate failed "+e)
 	
+	# this happens if no cached token found, or existing token was invalid
+	url = 'https://%s/aaa/v1/auth?grant_type=password'%host
 	payload = {
                 "username": username,
                 "password": password,
@@ -114,27 +151,46 @@ def get_auth(host, username, password):
 		if response.status_code == 201:
 			if  response.json().get('access_token'):
 				tokens[host]=response.json()
+				logger.info("Got token ok")
 				return response.json()
-		print response.content
-	except:
-		print "failed to post get new token"
+		logger.error("Failed get a token "+response.content)
+		return None
+	except Exception as e:
+		logger.error("Post for token get failed "+e)
 		return None
 
+#
+# calls HX API
+#
 def get_stats(authdata, url):
-	print "call get_stats"
+	logger.info("call for get_stats")
 	try:
 		headers = {'Authorization': authdata['token_type'] + ' ' + authdata['access_token'],'Connection':'close'}
                 response = requests.get(url,headers=headers,verify=False,timeout=4)
 		if response.status_code == 200:
+			logger.info("Got data ok")
 			return response.json()
-        	print response.content
-	except:
+		logger.error("Failed to get data "+response.content)
+		return None
+	except Exception as e:
+                logger.error("Post for data failed "+e)
                 return None
 
 def date_txt(epoch):
 	return  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch))
 	
 if __name__ == '__main__':
-	#app.run(host='0.0.0.0', port=8082, threaded=True)
-	http_server = WSGIServer(('10.100.253.13', 8082), app)
+	print "Service Started"
+	# Enable logging
+	logger = logging.getLogger("HX-Stats")
+    	logger.setLevel(logging.DEBUG)
+    	handler = logging.handlers.RotatingFileHandler(logFile, maxBytes=logBytes, backupCount=logCount)
+    	formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    	handler.setFormatter(formatter)
+    	logger.addHandler(handler)
+	logger.info("-"*25)
+    	logger.info("HX Stats script started")
+	
+	http_server = WSGIServer(('10.100.253.13', 8083), app, log = logger)
 	http_server.serve_forever()
+		
